c62aac8aba04508d914bd27e6e263f6b138d95c4
AlenCirikovic <alen.ciriko@gmail.com>
2025-03-27 23:01:44 +0100

    Implemented database module into the app module scope with the NestModule that configures the already made logger middle ware

--- src/modules/app.module.ts
+++ src/modules/app.module.ts
@@ -1,6 +1,8 @@
-import { Module } from '@nestjs/common'
+import { MiddlewareConsumer, Module, NestModule, RequestMethod } from '@nestjs/common'
 import { ConfigModule } from '@nestjs/config'
 import { configValidationSchema } from 'config/schema.config'
+import { DatabaseModule } from './database/database.module'
+import { LoggerMiddleWare } from 'middleware/logger.middleware'
 
 @Module({
   imports: [
@@ -9,8 +11,13 @@ import { configValidationSchema } from 'config/schema.config'
       envFilePath: [`.env.${process.env.STAGE}`],
       validationSchema: configValidationSchema,
     }),
+    DatabaseModule,
   ],
   controllers: [],
   providers: [],
 })
-export class AppModule {}
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(LoggerMiddleWare).forRoutes({ path: '*', method: RequestMethod.ALL })
+  }
+}
