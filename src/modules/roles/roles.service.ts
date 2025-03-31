cc1badadd64b1e2a50184cd3382e5fa7ee17aea0
AlenCirikovic <alen.ciriko@gmail.com>
2025-03-31 22:24:54 +0200

    Created roles dto and update dto together and implemented create method in roles service for creating new roles.

--- src/modules/roles/roles.service.ts
+++ src/modules/roles/roles.service.ts
@@ -1,4 +1,24 @@
-import { Injectable } from '@nestjs/common'
+import { BadRequestException, Injectable } from '@nestjs/common'
+import { InjectRepository } from '@nestjs/typeorm'
+import { Role } from 'entities/role.entity'
+import Logging from 'library/Logging'
+import { AbstractService } from 'modules/common/abstract.service'
+import { Repository } from 'typeorm'
+import { CreateUpdateRoleDto } from './dto/create-update-role.dto'
 
 @Injectable()
-export class RolesService {}
+export class RolesService extends AbstractService {
+  constructor(@InjectRepository(Role) private readonly rolesRepository: Repository<Role>) {
+    super(rolesRepository)
+  }
+
+  async create(createRolesDto: CreateUpdateRoleDto, permissionsIds: { id: string }[]): Promise<Role> {
+    try {
+      const role = this.rolesRepository.create({ ...createRolesDto, permissions: permissionsIds })
+      return this.rolesRepository.save(role)
+    } catch (error) {
+      Logging.error(error)
+      throw new BadRequestException('Something went wrong while creating a new permission.')
+    }
+  }
+}
