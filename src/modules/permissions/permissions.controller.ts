fb6029f3624cb80e138ad40c5004ff7a328fdc6c
AlenCirikovic <alen.ciriko@gmail.com>
2025-03-31 15:16:21 +0200

    Added create and findAll request handling in permissions controller aswell as the methods in the permission service.

--- src/modules/permissions/permissions.controller.ts
+++ src/modules/permissions/permissions.controller.ts
@@ -1,4 +1,21 @@
-import { Controller } from '@nestjs/common'
+import { Controller, Get, Post, HttpCode, HttpStatus, Body } from '@nestjs/common'
+import { PermissionsService } from './permissions.service'
+import { CreatePermissionDto } from './dto/create-permission.dto'
+import { Permission } from 'entities/permission.entity'
 
 @Controller('permissions')
-export class PermissionsController {}
+export class PermissionsController {
+  constructor(private readonly permissionsService: PermissionsService) {}
+
+  @Get()
+  @HttpCode(HttpStatus.OK)
+  async findAll(): Promise<Permission[]> {
+    return this.permissionsService.findAll()
+  }
+
+  @Post()
+  @HttpCode(HttpStatus.CREATED)
+  async create(@Body() createPermissionDto: CreatePermissionDto): Promise<Permission> {
+    return this.permissionsService.create(createPermissionDto)
+  }
+}
