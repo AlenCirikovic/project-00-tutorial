866178f789f1d8ef4b336dd2fa72dce5a6b4091c
AlenCirikovic <alen.ciriko@gmail.com>
2025-03-31 22:30:06 +0200

    Added method to Roles service called update, for updating active role permissions.

--- src/modules/roles/roles.service.ts
+++ src/modules/roles/roles.service.ts
@@ -1,10 +1,11 @@
-import { BadRequestException, Injectable } from '@nestjs/common'
+import { BadRequestException, Injectable, InternalServerErrorException } from '@nestjs/common'
 import { InjectRepository } from '@nestjs/typeorm'
 import { Role } from 'entities/role.entity'
 import Logging from 'library/Logging'
 import { AbstractService } from 'modules/common/abstract.service'
 import { Repository } from 'typeorm'
 import { CreateUpdateRoleDto } from './dto/create-update-role.dto'
+import { Permission } from 'entities/permission.entity'
 
 @Injectable()
 export class RolesService extends AbstractService {
@@ -18,7 +19,19 @@ export class RolesService extends AbstractService {
       return this.rolesRepository.save(role)
     } catch (error) {
       Logging.error(error)
-      throw new BadRequestException('Something went wrong while creating a new permission.')
+      throw new BadRequestException('Something went wrong while creating a new role.')
+    }
+  }
+
+  async update(roleId: string, updateRoleDto: CreateUpdateRoleDto, permissionsIds: { id: string }[]): Promise<Role> {
+    const role = (await this.findById(roleId)) as Role
+    try {
+      role.name = updateRoleDto.name
+      role.permissions = permissionsIds as Permission[]
+      return this.rolesRepository.save(role)
+    } catch (error) {
+      Logging.error(error)
+      throw new InternalServerErrorException('Something went wrong while updating the role.')
     }
   }
 }
