54801365a970e922532fc983676485373deba0ee
AlenCirikovic <alen.ciriko@gmail.com>
2025-03-31 13:37:14 +0200

    Added Login method in Auth Controller for handling login requests.

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -5,13 +5,20 @@ import {
   HttpCode,
   HttpStatus,
   Post,
+  Req,
+  Res,
+  UseGuards,
   UseInterceptors,
 } from '@nestjs/common'
 import { Public } from 'decorators/public.decorator'
 import { User } from 'entities/user.entity'
 
+import { Request, Response } from 'express'
+
 import { AuthService } from './auth.service'
 import { RegisterUserDto } from './dto/register-user.dto'
+import { LocalAuthGuard } from './guards/local-auth.guard'
+import { RequestWithUser } from 'interfaces/auth.interface'
 
 @Controller('auth')
 @UseInterceptors(ClassSerializerInterceptor)
@@ -23,5 +30,15 @@ export class AuthController {
   @HttpCode(HttpStatus.CREATED)
   async register(@Body() body: RegisterUserDto): Promise<User> {
     return this.authService.register(body)
+  }
+
+  @Public()
+  @UseGuards(LocalAuthGuard)
+  @Post('login')
+  @HttpCode(HttpStatus.OK)
+  async login(@Req() req: RequestWithUser, @Res({ passthrough: true }) res: Response): Promise<User> {
+    const access_token = await this.authService.generateJwt(req.user)
+    res.cookie('access_token', access_token, { httpOnly: true })
+    return req.user
   }
 }
